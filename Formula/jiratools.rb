aa9bb018b3b3528186ac583bba5690710807253b
Shane Zatezalo <shane@lottadot.com>
2016-08-18 23:21:30 -0400

    Updated Jiratools SHA

--- Formula/jiratools.rb
+++ Formula/jiratools.rb
@@ -5,7 +5,7 @@ class Jiratools < Formula
   homepage "https://github.com/lottadot/jiratools"
   url "https://github.com/lottadot/jiratools/archive/0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "23438f08548b9a8c8a5bf907122ec0ec00b2c7791f623fb3b1d8a517ac69c100"
+  sha256 "eaf22f122aae533893770e8133d834352b5a81e414481d947f0e29ada2399207"
 
   depends_on :xcode => ["7.3", :build]
   depends_on 'xcproj' => :recommended
