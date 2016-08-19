f6b5f9850902c9afda68ae0ed1eba0f646701c8f
Shane Zatezalo <shane@lottadot.com>
2016-08-18 22:50:16 -0400

    Update JiraTools formulae
    * Add require
    * Add xcproj recommended
    * Update test to use --version

--- Formula/jiratools.rb
+++ Formula/jiratools.rb
@@ -1,3 +1,5 @@
+require 'formula'
+
 class Jiratools < Formula
   desc "Collection of command-line tool(s) for Jira."
   homepage "https://github.com/lottadot/jiratools"
@@ -6,12 +8,13 @@ class Jiratools < Formula
   sha256 "23438f08548b9a8c8a5bf907122ec0ec00b2c7791f623fb3b1d8a517ac69c100"
 
   depends_on :xcode => ["7.3", :build]
+  depends_on 'xcproj' => :recommended
 
   def install
     system "make", "prefix_install"
   end
 
   test do
-    system bin/"jiratools"
+    system bin/"jiratools", "--version"
   end
 end
