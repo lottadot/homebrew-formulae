dc8e398a8be929927a6f8b0d30e7b6969fca16b9
Shane Zatezalo <shane@lottadot.com>
2016-09-27 07:47:55 -0400

    Update for Jiratools 0.3.1

--- Formula/jiratools.rb
+++ Formula/jiratools.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Jiratools < Formula
   desc "Collection of command-line tool(s) for Jira."
   homepage "https://github.com/lottadot/jiratools"
-  url "https://github.com/lottadot/jiratools/archive/0.3.0.tar.gz"
-  version "0.3.0"
-  sha256 "31edb819a855545472feed753c99db6369f5fc6c2141a1539db40ad3e1aa8d1a"
+  url "https://github.com/lottadot/jiratools/archive/0.3.1.tar.gz"
+  version "0.3.1"
+  sha256 "ef97bfdbd3a7e29c1f3368a91af805ee53b59cdb6b8cb896c14c6ce9f3113ccb"
 
   depends_on :xcode => ["8.0", :build]
   depends_on 'xcproj' => :recommended
