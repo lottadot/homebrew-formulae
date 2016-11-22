e27895e83ac5f0827d6e89d640cafe6ca295363c
Shane Zatezalo <shane@lottadot.com>
2016-11-22 08:10:04 -0500

    Fix for Jiratools 0.3.3 filename/SHA

--- Formula/jiratools.rb
+++ Formula/jiratools.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Jiratools < Formula
   desc "Collection of command-line tool(s) for Jira."
   homepage "https://github.com/lottadot/jiratools"
-  url "https://github.com/lottadot/jiratools/archive/0.3.2.tar.gz"
+  url "https://github.com/lottadot/jiratools/archive/0.3.3.tar.gz"
   version "0.3.3"
   # shasum -a 256
   sha256 "e0d9a9a347386b4c1861d37556d532e9fa10c2a82bf4e1858408331f08808c04"
